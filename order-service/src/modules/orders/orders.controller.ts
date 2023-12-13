1449f47661ed097179c3cfef4e8b05ab9d56182e
Tân Nguyễn <tannguyenit95@gmail.com>
2023-12-13 22:47:33 +0700

    [OrderService] Get user detail via NATS:

--- order-service/src/modules/orders/orders.controller.ts
+++ order-service/src/modules/orders/orders.controller.ts
@@ -5,9 +5,9 @@ import {
   Get,
   HttpCode,
   HttpStatus,
+  Inject,
   Param,
   Post,
-  Put,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
@@ -16,11 +16,17 @@ import type { OrdersDto } from './dtos/orders.dto';
 import { OrdersService } from './orders.service';
 import { OrderCreateDto } from './dtos/order.create.dto';
 import { UserEntity } from './../../modules/user/user.entity';
+import { ClientProxy } from '@nestjs/microservices';
+import { uniq } from 'lodash';
+import { lastValueFrom } from 'rxjs';
 
 @Controller('orders')
 @ApiTags('orders')
 export class OrdersController {
-  constructor(private ordersService: OrdersService) {}
+  constructor(
+    private ordersService: OrdersService,
+    @Inject('USER_SERVICE') private readonly userClient: ClientProxy
+    ) {}
 
   @Post()
   @Auth([])
@@ -46,14 +52,22 @@ export class OrdersController {
   @HttpCode(HttpStatus.OK)
   async getAllOrders(@AuthUser() user: UserEntity): Promise<any> {
     const allOrder = await this.ordersService.getAllOrders({ user });
+    const allUserID = allOrder.map(i => i.customerId)
+    const allUser = await lastValueFrom(this.userClient.send('get_user_by_ids', { ids: uniq(allUserID)}))
+    
     return allOrder.map(i => {
+      const user = allUser.find(u => u.id == i.customerId)
+      if (!user) {
+        return null
+      }
+
       return {
         ...i,
         customer: {
           name: user.displayName,
           email: user.email,
-          avatarUrl: 'https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg',
-          ipAddress: '192.158.1.38',
+          avatarUrl: user.photoURL,
+          ipAddress: '127.0.0.1',
         }
       }
     })
@@ -62,27 +76,21 @@ export class OrdersController {
   @Get(':id')
   @Auth([])
   @HttpCode(HttpStatus.OK)
-  async getSingleOrders(@AuthUser() user: UserEntity, @Param('id') id: number): Promise<OrdersDto> {
+  async getSingleOrders(@Param('id') id: number): Promise<OrdersDto> {
     const entity = await this.ordersService.getSingleOrders(id);
+
+    const user = await lastValueFrom(this.userClient.send('get_user_by_ids', { ids: [entity.customerId]}))
+
     Object.assign(entity, { 
       orderNumber: `#${id}`,
       customer: {
         name: user.displayName,
         email: user.email,
-        avatarUrl: 'https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg',
-        ipAddress: '192.158.1.38',
+        avatarUrl: user.photoURL,
+        ipAddress: '127.0.0.1',
       }
     })
     return entity;
-  }
-
-  @Put(':id')
-  @HttpCode(HttpStatus.ACCEPTED)
-  updateOrders(
-    @Param('id') id: number,
-    @Body() updateOrdersDto: any,
-  ): Promise<void> {
-    return this.ordersService.updateOrders(id, updateOrdersDto);
   }
 
   @Delete(':id')
--- order-service/src/modules/orders/orders.module.ts
+++ order-service/src/modules/orders/orders.module.ts
@@ -4,11 +4,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrdersController } from './orders.controller';
 import { OrdersEntity } from './orders.entity';
 import { OrdersService } from './orders.service';
-
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([OrdersEntity]),
+    ClientsModule.register([
+      {
+        name: 'USER_SERVICE',
+        transport: Transport.NATS,
+        options: {}
+      },
+    ]),
   ],
   providers: [OrdersService],
   controllers: [OrdersController],
